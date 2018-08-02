f66517a0300e68ac20057c32d790ec9375230bc4
Jace Babin <jbabin91@gmail.com>
2018-08-02 13:30:50 -0700

    Finished keyboard event section

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ new Vue({
   el: '#app',
   data: {
     title: 'Becoming a Vue ninja',
+    name: 'John',
   },
-  methods: {},
+  methods: {
+    updateName(e) {
+      // console.log(e.target.value);
+      this.name = e.target.value;
+    },
+  },
 });
