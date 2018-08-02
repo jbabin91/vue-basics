5e1b35c7b33b5d40fcb728df79d1054111245c36
Jace Babin <jbabin91@gmail.com>
2018-08-02 14:17:04 -0700

    Finished v-for section

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ new Vue({
   el: '#app',
   data: {
     title: 'Becoming a Vue ninja',
+    items: ['Mushroom', 'Green Shells', 'Red Shells', 'Banana', 'Start'],
+    ninjas: [
+      { name: 'Crystal', age: 25, belt: 'Black' },
+      { name: 'Ryu', age: 30, belt: 'Brown' },
+      { name: 'Ken', age: 35, belt: 'Orange' },
+    ],
   },
   methods: {},
 });
